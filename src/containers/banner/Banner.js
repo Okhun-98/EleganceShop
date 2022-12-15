f2134c4ec6f8ab03468c5e34bc8efdd9b75aeb90
Okhundzhon Abdulloev <okhunabd@gmail.com>
2022-12-15 22:44:02 +0100

    add responsibly style

--- src/containers/banner/Banner.js
+++ src/containers/banner/Banner.js
@@ -4,16 +4,12 @@ import bannerImg from "../../images/muslim1.jpeg"
 import "./Banner.css"
 
 export const Banner = () => {
-    const [count, setCount] = useState(0)
     return (
         <div className="banner">
             <div className="banner-data">
-                <h1 className="banner-title">Make Your Orders {count}</h1>
+                <h1 className="banner-title">Make Your Orders</h1>
                 <p className="banner-paragraph">You can increase the ovary or reduce or pay</p>
                 <Button url="/basket" title="Go to basket" myStyle={{ marginLeft: "25px" }} />
-                <Button title="Minus" myClick={() => setCount(count - 1)} myStyle={{ marginLeft: "25px", backgroundColor: "red", marginTop: "10px" }} />
-                <Button title="Plus" myClick={() => setCount(count + 1)} myStyle={{ marginLeft: "25px", backgroundColor: "green", marginTop: "10px" }} />
-
             </div>
             <img className="banner-img" src={bannerImg} alt="Img of banner" />
         </div>
--- src/containers/navbar/Navbar.js
+++ src/containers/navbar/Navbar.js
@@ -1,11 +1,14 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { Context } from "../../context/Context"
 import "./Navbar.css"
 
 export function Navbar() {
+    const { valueBasket, setValueBasket } = useContext(Context)
     return (
         <div className="navbar">
-            <Link className="link" to="/">Home</Link>
-            <Link className="link" to="basket">Basket</Link>
+            <Link className="link" to="/">Home </Link>
+            <Link className="link" to="basket">Basket <span className="count-basket">{valueBasket?.length}</span></Link>
         </div>
     )
 }
